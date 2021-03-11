27bf530f671c9e54a20f5104e7280a509c26994a
rui-watanabe <wataru610806@gmail.com>
2021-03-11 21:35:58 +0900

    prop array to html

--- hello.go
+++ hello.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Todo struct {
+	CreatedAt time.Time
+	CreatedBy string
+	Content   string
+	Status    int
+}
+
 func main() {
 	// fmt.Println("hello world")
-	todo := []string{
-		"JavaScript",
-		"TypeScript",
-		"Golang",
-		"React",
+	// todo := make([]Todo, 0, 5)
+	// t := Todo {
+	// 	CreatedAt: time.Now(),
+	// 	CreatedBy: "max",
+	// 	Content:   "hello",
+	// 	Status:    0,
+	// }
+	// todo = append(todo, t)
+	todo := []Todo{
+		Todo{
+			CreatedAt: time.Now(),
+			CreatedBy: "max",
+			Content:   "hello",
+			Status:    0,
+		},
+		Todo{
+			CreatedAt: time.Now(),
+			CreatedBy: "leo",
+			Content:   "hi",
+			Status:    0,
+		},
+		Todo{
+			CreatedAt: time.Now(),
+			CreatedBy: "gin",
+			Content:   "good morning",
+			Status:    0,
+		},
 	}
+
 	r := gin.Default()
 	r.LoadHTMLFiles("./template/index.html")
 	r.GET("/ping", func(c *gin.Context) {
