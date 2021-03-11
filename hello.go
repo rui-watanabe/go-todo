f7488aa4f572b3c33547ee9d8862d89579f2d54d
rui-watanabe <wataru610806@gmail.com>
2021-03-11 21:18:10 +0900

    indicate todos

--- hello.go
+++ hello.go
@@ -8,10 +8,22 @@ import (
 
 func main() {
 	// fmt.Println("hello world")
+	todo := []string{
+		"JavaScript",
+		"TypeScript",
+		"Golang",
+		"React",
+	}
 	r := gin.Default()
+	r.LoadHTMLFiles("./template/index.html")
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+	r.GET("/todo", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", map[string]interface{}{
+			"todo": todo,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
